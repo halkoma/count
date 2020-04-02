aaffc65001956f48a99db38eff6f4263d12aed4e
halkoma <46521200+halkoma@users.noreply.github.com>
2020-04-02 09:39:33 +0300

    count.py now works with multiple files. Add test_data2.txt.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # count.py
-
-Usage with test data: ./count.py test_data.txt
+Calculates how much each payee has paid from one or more Nordea .txt files.
+Prints the file name before results.
+Can be used for example like:
+`./count.py test_data.txt test_data2.txt >> all_results.txt`
--- count.py
+++ count.py
@@ -1,72 +1,88 @@
 #!/usr/bin/python3
 """
-Calculate how much each payee has paid from a .txt gotten from Nordea
+Calculate how much each payee has paid from one or more Nordea .txt files
+Prints the file name before results
+Can be used for example like:
+./count.py test_data.txt test_data2.txt >> all_results.txt
 """
 
 import re
 import sys
 
-data_people = []
-names = []
-txt_file = ""
+def print_results(data_people_sorted, file_name):
+    """Print the results for example like follows.
 
-def print_results(data_people_sorted):
+    FILE_NAME.TXT
+
+    PERSON 1 NAME             : 100.0
+    PERSON 2 NAME             : -25.55
+    PERSON 3 WITH LONGER NAME : 15.0
+    """
+    print(file_name.upper(), '\n')
     for i in data_people_sorted:
         print(i["name"].ljust(len(max(names, key=len))) + " : " + str(i["amount"]))
+    print()
 
 def parseFields(line):
-    #return date, amount and name
+    """Return date, amount and name"""
     fields = re.split(r'\t+', line)
     date, amount, name = fields[2:5]
     amount = float(amount.replace(',', '.'))
     return date, amount, name
 
-def main():
+def add_or_edit_person(date, amount, name):
+    """Add or edit person in data_people.
+
+    if not person in data_people, add person to data_people
+    if person in data_people, update the paid amount
+    """
+    if name not in names:
+        newPerson = {}
+        newPerson.update({"name": name})
+        newPerson.update({"amount": amount})
+        newPerson.update({"date": date})
+        data_people.append(newPerson)
+        names.append(name)
+    else:
+        editPerson = next(filter(lambda person: person['name'] == name,
+                                 data_people))
+        editPerson["amount"] += amount
+
+def main(argv):
     global data_people
     global names
-    global txt_file
     try:
-        txt_file = sys.argv[1]
+        txt_files = argv[1:]
     except:
         print("Usage so far: ./count.py test_data.txt")
 
-    try:
-        with open(txt_file, encoding='utf-8') as f:
-            #extract date, amount and name from each line
-            #if not person in data_people, add people to data_people
-            #if person in data_people, update the paid amount
-            #also maintain names-list making it easier to check if the names already exist
+    for txt_file in txt_files:
+        data_people = []
+        names = []
+        try:
+            with open(txt_file, encoding='utf-8') as f:
+                #extract date, amount and name from each line
+                #also maintain names-list making it easier to check if the names already exist
 
-            #skip headers
-            next(f), next(f), next(f)
+                #skip headers
+                next(f), next(f), next(f)
 
-            for line in f:
-                ##initial handling
-                #skip empty lines
-                if not line.strip():
-                    continue
-                date, amount, name = parseFields(line)
+                for line in f:
+                    ##initial handling
+                    #skip empty lines
+                    if not line.strip():
+                        continue
+                    date, amount, name = parseFields(line)
+                    add_or_edit_person(date, amount, name)
 
-                #add or edit person in data_people
-                if name not in names:
-                    newPerson = {}
-                    newPerson.update({"name": name})
-                    newPerson.update({"amount": amount})
-                    newPerson.update({"date": date})
-                    data_people.append(newPerson)
-                    names.append(name)
-                else:
-                    editPerson = next(filter(lambda person: person['name'] == name,
-                                             data_people))
-                    editPerson["amount"] += amount
-    except Exception as e:
-        print(e)
+                try:
+                    data_people_sorted = sorted(data_people, key=lambda person: person["name"])
+                    print_results(data_people_sorted, f.name)
+                except Exception as e:
+                    print(e)
 
-    try:
-        data_people_sorted = sorted(data_people, key=lambda person: person["name"])
-        print_results(data_people_sorted)
-    except Exception as e:
-        print(e)
+        except Exception as e:
+            print(e)
 
 if __name__== "__main__":
-    main()
+    main(sys.argv)
